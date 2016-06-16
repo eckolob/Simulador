e9b69f7b93ff608c166ecf85a12ef7cce4638ae8
eckolob <ecko_lob@outlook.com>
2016-06-15 19:26:51 -0500

    Practica 1
    Falta que se asignen los procesos correspondientes a cada lote
    Falta que se muestre en base al reloj y se actualize

--- Simulador.java
+++ Simulador.java
@@ -1,82 +1,167 @@
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
  
 public class Simulador {
  	Lote lote = new Lote();	
         Proceso p=new Proceso();
+        Interfaz ventana =new Interfaz();
        
-    boolean tal=false;
-    
+    boolean tal=false,error=false;
+    //Esta array list es para extraer los lotes de donde se almacenan y mostrarlos
+    ArrayList <Proceso> temporal;
+    //En estas array list se guardan los procesos y lotes
     ArrayList <Lote> Lotes;
+    ArrayList <Proceso> LoteTemporal;
     ArrayList <Proceso> Procesos;
     int reloj;
-    int lotesrequeridos=0;
+    int lotesreq=0;
     int num1,num2;
-    String operacion="";
   
     boolean termina=false;
-      int id,tme,procesosrecibidos; String programador;
+      int id,tme,procesosrecibidos; String programador,resultado;
     //Contador que lleva el tiempo (determinar mi unidad de tiempo
     //ejemplo 1 tme = x segundos controlados por un delay
       
       
+      
+      
+      
       public Simulador()
       {
-         Lotes= new ArrayList<> (); 
+          //quiero capturar n objetos, dividir n/5, crear un array list que
+          Lotes= new ArrayList<> (); 
          Procesos= new ArrayList<> (); 
+         temporal = new ArrayList();
+         LoteTemporal = new ArrayList();
+         
+         
       }
+      
+      
+      
+      	public String Sumar(int n1,int n2)
+	{
+		int suma= n1+n2;
+		resultado=""+n1+" + "+n2+" = "+suma;
+		return  resultado;
+	}
+	
+	public String Restar(int n1,int n2)
+	{
+		int resta= n1-n2;
+		resultado=""+n1+" - "+n2+" = "+resta;
+		return  resultado;
+	}
+	
+	public String Multiplicar(int n1,int n2)
+	{
+		int mult= n1*n2;
+		resultado=""+n1+" * "+n2+" = "+mult;
+		
+		return  resultado;
+	}
+	
+	public String Dividir(int n1,int n2)
+	{
+		if(n2<=0)
+		{
+		 	JOptionPane.showMessageDialog(null, "Error: Division entre 0!", "", JOptionPane.ERROR_MESSAGE);
+                        error=true;
+		}
+		else
+		{
+		 int div= n1/n2;
+		 resultado=""+n1+" / "+n2+" = "+div;	
+		}
+	
+		
+		return  resultado;
+	}
+	
+	public String Residuo(int n1,int n2)
+	{
+		if(n2<=0)
+		{
+		 	
+                        JOptionPane.showMessageDialog(null, "Division entre 0!", "", JOptionPane.ERROR_MESSAGE);
+                        error=true;
+		}
+		else
+		{
+		 int residuo= n1/n2;
+		 resultado=""+n1+" / "+n2+" = "+residuo;	
+		}
+		return  resultado;
+	}
+	
+	public String Potencia(int n1,int n2)
+	{
+	 int potencia=(int) Math.pow(n1, n2);;
+		resultado=""+n1+" ^ "+n2+" = "+potencia;
+		return  resultado;	
+	}
  
     public void menu(){
   
     	int cuentaprocesos=0;
         String opcion="0";
-    
+        
     	
         String op;
-          procesosrecibidos = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el numero de procesos: "));
-          lotesrequeridos=lote.CalcularLotes(procesosrecibidos);
+
+        procesosrecibidos = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el numero de procesos: "));
+         lotesreq=lote.CalcularLotes(procesosrecibidos); 
           
           
        do{
-           op = JOptionPane.showInputDialog(null,"2.  Introducir datos\n"
-                                              +"3.  Modificar datos\n"
-                                              +"4.  Eliminar datos\n"
-                                              +"5.  Buscar datos\n"
-                                              +"6.  Mostrar datos\n"
-                                              +"7.  Salir");
-                                              
- 			
-           switch(op){
-               case "2"://Bloque para introducir datos al array list
-                     
+                                   
+ 		reloj++;	
+ 
+               //Bloque para introducir datos al array list
+                       
                         if(cuentaprocesos==procesosrecibidos)
            	{
            		 termina=true;
-                         JOptionPane.showMessageDialog(null, "Ya no puedes ingresar mas procesos!", "", JOptionPane.ERROR_MESSAGE);
+                       //  JOptionPane.showMessageDialog(null, "Ya no puedes ingresar mas procesos!", "", JOptionPane.ERROR_MESSAGE);
                         break;
            	}
-                        
+                      
  						
                         id = Integer.parseInt(JOptionPane.showInputDialog(null, "ID: "));
+                        
+                        for(int z=0;z<Procesos.size();z++)
+                        {
+                            while(id==Procesos.get(z).ObtenerId())
+                                {
+                                    JOptionPane.showMessageDialog(null, "Id Duplicado!", "", JOptionPane.ERROR_MESSAGE);
+                                     id = Integer.parseInt(JOptionPane.showInputDialog(null, "ID: "));
+                                }
+                        }
+                        
+                        if(error==false)
+                        {
+                            
                         programador = JOptionPane.showInputDialog(null, "Programador: ");
                         tme = Integer.parseInt(JOptionPane.showInputDialog(null, "TME: "));
                         num1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Numero: "));
                         num2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Numero: "));
-                        opcion = JOptionPane.showInputDialog(null, "1)Suma\n2)Resta\n3)Multiplicacion\n4)Division\n5)Residuo\n6)Potencia: ");
+                         opcion = JOptionPane.showInputDialog(null, "1)Suma\n2)Resta\n3)Multiplicacion\n4)Division\n5)Residuo\n6)Potencia: ");
                         switch(opcion)
                         {
-                        	case "1":operacion=p.Sumar(num1,num2);
+                            
+                        	case "1":resultado=Sumar(num1,num2);
                         		break;
-                        	case "2":operacion=p.Restar(num1,num2);
+                        	case "2":resultado=Restar(num1,num2);
                         		break;
-                        	case "3":operacion=p.Multiplicar(num1,num2);
+                        	case "3":resultado=Multiplicar(num1,num2);
                         		break;
-                        	case "4":operacion=p.Dividir(num1,num2);
+                        	case "4":resultado=Dividir(num1,num2);
                         		break;
-                        	case "5":operacion=p.Residuo(num1,num2);
+                        	case "5":resultado=Residuo(num1,num2);
                         		break;
-                        	case "6":operacion=p.Potencia(num1,num2);
+                        	case "6":resultado=Potencia(num1,num2);
                         		break;
                         
                         		default:
@@ -84,102 +169,68 @@ public class Simulador {
                         }
                         
                         //Agregar objeto a la lista
-                         p=new Proceso(id,programador,tme,operacion);
-                        Procesos.add(p);  
-                        cuentaprocesos++;
-                          
-                            // Procesos.add(new Proceso(id,programador,tme,operacion));
+                        //Aqui se borro el lote que tenia y se encima el nuevo
                         
+                        //La variable operacion tiene algo mal y reemplaza a la operacion anterior
+                         p=new Proceso(id,programador,tme,resultado);
                          
-                    
-                       	/*
-                        if(Procesos.size()<=lote.capacidad)
-                        {
-							int x=1;	
-								if(x!=lotesrequeridos)
-								{
-								 Lotes.add(new Lote(x,Procesos));	
-								 	x++;
-								}
-				
-                         	cuentaprocesos=0;
-                        }
-                       
-                      */
-                        	                       
-                                    
-                   break;
-               case "3"://Bloque para modificar datos al array list
-                   
-                        int indice, b;
-                        String c;
-                        b = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el indice(id) a Modificar: "));
-                        if(existeEnArray(b)){                            
-                            indice = indiceDato(b);
-                            c = JOptionPane.showInputDialog(null, "Programador: ");
-                            Procesos.get(indice).AsignarNombre(c); 
-                            	//Aqui se puede indicarle que modifique mas atributos invocando al metodo asignarATRIBUTO() creado en Proceso
-                        }else{
-                            JOptionPane.showMessageDialog(null, "No existe el dato a modificar !", "", JOptionPane.ERROR_MESSAGE);
-                        }
-                                 
-                   break;
-               case "4"://Bloque para eliminar dato del array
-                   
-                   
-                        id = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el Id a eliminar: "));
-                        for(int i=0; i<Procesos.size(); i++){
-                            if(Procesos.get(i).ObtenerId()==id){
-                                Procesos.remove(i);
-                            }
-                        }
-                     
-                   break;
-               case "5"://Bloque para buscar datos en el array list
-                  
-                        String mostrarBus= "No se ha encontrado nada!";
+                         cuentaprocesos++;
+                          int l=1;
+                         
+                             Procesos.add(p);  
+      
                         
-                        id = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el Id a buscar: "));
-                        for( int i = 0 ; i  < Procesos.size(); i++){
-                        	//Obtener el id del objeto almacenado en la posicion 0,1,2,...
-                            if(Procesos.get(i).ObtenerId()==id){
-                                mostrarBus  ="";
-                                mostrarBus += "Id: "+Procesos.get(i).ObtenerId();
-                                mostrarBus += "\nNombre "+Procesos.get(i).ObtenerProgramador();
-                                mostrarBus += "\nOperacion "+Procesos.get(i).ObtenerOperacion();
-                                mostrarBus += "\nTME: "+Procesos.get(i).ObtenerTME();
-                                mostrarBus += "\n"+"\n";
-                            }                        
+                            lote=new Lote(l,Procesos);
+                             
+                            Lotes.add(lote);//agregar lote al arraylist de lotes
+                         //   Procesos.clear();
+                            
+         
                        }
-                         JOptionPane.showMessageDialog(null, mostrarBus);
-                  
- 
-                   break;
-               case "6"://Bloque para mostar los datos del array list
-                  
-                       String Salida= "";
-                       for( int i = 0 ; i  < Procesos.size(); i++){
-                        Salida += "Id: "+Procesos.get(i).ObtenerId();
-                        Salida += "\nProgramador: "+Procesos.get(i).ObtenerProgramador();
-                        Salida += "\nOperacion: "+Procesos.get(i).ObtenerOperacion();
-                        Salida += "\nTME: "+Procesos.get(i).ObtenerTME();
-                        Salida += "\n"+"\n";
-                       }
-                        JOptionPane.showMessageDialog(null, Salida);
-                                    
-                   break;                   
-               case "7":
-                   System.exit(0);
-                   break;
-               default:
-                   JOptionPane.showMessageDialog(null, "Opcion invalida !");
-                   break;
-           }      
-           	
-           	
-       }while(!op.equals("7")||termina!=true);
+                                  
+                         	
+       }while(cuentaprocesos<=procesosrecibidos||error!=true);
        
-     
+       
+    //Ver lote
+                           ventana.Simular();
+                           
+               
+                      //Mostrar lote completo
+                       for( int m = 0 ; m  < Lotes.size(); m++)
+                       {
+                        Lotes.get(m).ObtenerIdLote();
+                       temporal= Lotes.get(m).ObtenerProcesos();
+                       }
+                       
+                         for( int i = 0 ; i  < temporal.size(); i++)
+                         {
+                         ventana.txt_programador.setText(temporal.get(i).ObtenerProgramador());
+                         ventana.txt_idproceso2.setText(Integer.toString(temporal.get(i).ObtenerId()));  
+                           ventana.txt_operacion.setText(temporal.get(i).ObtenerOperacion());
+                           ventana.txt_tme2.setText(Integer.toString(temporal.get(i).ObtenerTME()));
+                           
+                           ventana.txt_loteEjecutandose.setText(Integer.toString(Lotes.get(i).idLote));
+                           
+                           ventana.ta_loteTerminado.append(Integer.toString(Lotes.get(i).idLote));
+                           ventana.ta_loteTerminado.append("               "+temporal.get(i).ObtenerOperacion()+"\t");
+                           ventana.ta_loteTerminado.append(Integer.toString(temporal.get(i).ObtenerId()));
+                            ventana.ta_loteTerminado.append("\n");
+                 
+                           ventana.ta_loteActual.append("         "+ventana.txt_idproceso2.getText()+"\t   "+ventana.txt_tme2.getText()+"\n"); 
+                           ventana.ta_loteActual.setLineWrap(true); 
+                           ventana.ta_loteActual.setWrapStyleWord(true);
+                                             
+                       
+                           
+                           
+                       }
+                            
+                            ventana.txt_reloj.setText(Integer.toString(reloj));
+                   
+    
+    SwingUtilities.updateComponentTreeUI(ventana);                
+                    
       
        
        
